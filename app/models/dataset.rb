e1a321ce1d5b268647df29fb289b685c6c80ddba
jiikko <n905i.1214@gmail.com>
2021-05-19 12:21:36 +0900

    digestをとる

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -2,4 +2,12 @@ class Dataset < ApplicationRecord
   has_one_attached :image
 
   has_many :dataset_positions, dependent: :destroy
+
+  before_save do
+    save_digest
+  end
+
+  def save_digest
+    self.digest = Digest::MD5.hexdigest(self.image.attachment.download)
+  end
 end
