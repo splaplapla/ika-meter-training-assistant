fe6b4103f98fa139de0bbebf711c8865bae6beca
jiikko <n905i.1214@gmail.com>
2021-05-21 09:21:36 +0900

    a

--- app/commands/create.rb
+++ app/commands/create.rb
@@ -1,10 +1,21 @@
 require "fileutils"
 
 class Create
-  def self.execute
-    `/opt/homebrew/opt/opencv@2/bin/opencv_createsamples -info positive.dat -vec positive.vec -w 100 -h 100 -num 2000`
-    num_pos = File.open("positive.dat").readlines.count
-    num_neg = File.open("negative.dat").readlines.count
-    `/opt/homebrew/opt/opencv@2/bin/opencv_traincascade -data tmp/model -vec positive.vec -bg negative.dat -numPos #{num_pos} -numNeg #{num_neg} -w 100 -h 100 -baseFormatSave`
+  class << self
+    def execute
+      puts "/opt/homebrew/opt/opencv@2/bin/opencv_createsamples -info positive.dat -vec positive.vec -w 100 -h 100 -num 2000"
+      FileUtils.mkdir_-p "tmp/model"
+      puts "/opt/homebrew/opt/opencv@3/bin/opencv_traincascade -data tmp/model -vec positive.vec -bg negative.dat -numPos #{num_pos} -numNeg #{num_neg} -w 100 -h 100 -baseFormatSave"
+    end
+
+    private
+
+    def num_pos
+      File.open("positive.dat").readlines.count
+    end
+
+    def num_neg
+      File.open("negative.dat").readlines.count
+    end
   end
 end
