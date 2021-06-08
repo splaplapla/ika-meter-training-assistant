fd61bcaada453f6a66307007e80b001b34637b75
jiikko <n905i.1214@gmail.com>
2021-06-08 20:31:12 +0900

    再評価するメソッドを生やす

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -13,6 +13,22 @@ class Dataset < ApplicationRecord
     dataset_positions.size > 0
   end
 
+  def r
+    min = OpenCV::CvSize.new(64, 64)
+    scale_factor = 1.01
+    detector = OpenCV::CvHaarClassifierCascade::load("/Users/koji/src/ika-meter-training-assistant/tmp/model/cascade.xml")
+    filename = ActiveStorage::Blob.service.send(:path_for, self.image.key)
+    image = OpenCV::CvMat.load(filename)
+    croped_image = Crop.ikatako_meter_with_padding(image)
+    ActiveRecord::Base.transaction do
+      self.dataset_temporary_positions.delete_all
+      ImageProcessor::DeathMarkOverrider.new(croped_image).override!
+      detector.detect_objects(croped_image, min_size: min, min_neighbors: 5, scale_factor: scale_factor).each do |rect|
+        self.dataset_temporary_positions.create!(x: rect.top_left.x + 450, y: rect.top_left.y, width: rect.bottom_right.x - rect.top_left.x, height: rect.bottom_right.y)
+      end
+    end
+  end
+
   private
 
   def save_digest
